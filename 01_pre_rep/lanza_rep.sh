65c09f81c8c7d8724ab54124d937cd5863c808f6
Carlos Souza <105952734+karurosu28c@users.noreply.github.com>
2024-04-25 16:58:24 -0300

    Update lanza_rep.sh

--- 01_pre_rep/lanza_rep.sh
+++ 01_pre_rep/lanza_rep.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
-#SBATCH -n 1
-#SBATCH --time 5-00:00:00
-#SBATCH -p gpu
-#SBATCH -J repTAL
-
-Exibe os nós alocados para o Job
-echo $SLURM_JOB_NODELIST
-nodeset -e $SLURM_JOB_NODELIST
-cd $SLURM_SUBMIT_DIR
-
-module load md/amber20
+#PBS -N pre-rep
+#PBS -l select=1:ncpus=1:ngpus=1
+#PBS -l walltime=100:00:00
+#PBS -q CCAD_QGPU
+#PBS -o output.prep
+#PBS -e error.prep
+#-----------------------------------#
+WORKDIR=$PBS_O_WORKDIR
+echo $WORKDIR
+cd $WORKDIR
+module load md/amber22
 
 export Step1Dir=../../01_pre_rep
 export Exec=pmemd.cuda
 export Parm=step3_input.parm7
 
-## Ultima etapa de equilibrio + produção de 100ns
-$Exec -O -i $Step1Dir/equil11.in -p $Step1Dir/$Parm -c $Step1Dir/equil10.rst -o equil11.out -r equil11.rst -x equil11.mdcrd -ref $Step1Dir/equil10.rst
+$Exec -O -i $Step1Dir/equil10.in -p $Step1Dir/$Parm -c $Step1Dir/equil9.rst -o equil10.out -r equil10.rst -x equil10.mdcrd -ref $Step1Dir/equil9.rst
 
-$Exec -O -i $Step1Dir/prod.in -p $Step1Dir/$Parm -c equil11.rst -o prod10ns.out -r prod10ns.rst -x prod10ns.mdcrd -ref equil11.rst
-$Exec -O -i $Step1Dir/prod.in -p $Step1Dir/$Parm -c prod10ns.rst -o prod20ns.out -r prod20ns.rst -x prod20ns.mdcrd -ref prod10ns.rst
-$Exec -O -i $Step1Dir/prod.in -p $Step1Dir/$Parm -c prod20ns.rst -o prod30ns.out -r prod30ns.rst -x prod30ns.mdcrd -ref prod20ns.rst
-$Exec -O -i $Step1Dir/prod.in -p $Step1Dir/$Parm -c prod30ns.rst -o prod40ns.out -r prod40ns.rst -x prod40ns.mdcrd -ref prod30ns.rst
-$Exec -O -i $Step1Dir/prod.in -p $Step1Dir/$Parm -c prod40ns.rst -o prod50ns.out -r prod50ns.rst -x prod50ns.mdcrd -ref prod40ns.rst
-$Exec -O -i $Step1Dir/prod.in -p $Step1Dir/$Parm -c prod50ns.rst -o prod60ns.out -r prod60ns.rst -x prod60ns.mdcrd -ref prod50ns.rst
-$Exec -O -i $Step1Dir/prod.in -p $Step1Dir/$Parm -c prod60ns.rst -o prod70ns.out -r prod70ns.rst -x prod70ns.mdcrd -ref prod60ns.rst
-$Exec -O -i $Step1Dir/prod.in -p $Step1Dir/$Parm -c prod70ns.rst -o prod80ns.out -r prod80ns.rst -x prod80ns.mdcrd -ref prod70ns.rst
-$Exec -O -i $Step1Dir/prod.in -p $Step1Dir/$Parm -c prod80ns.rst -o prod90ns.out -r prod90ns.rst -x prod90ns.mdcrd -ref prod80ns.rst
-$Exec -O -i $Step1Dir/prod.in -p $Step1Dir/$Parm -c prod90ns.rst -o prod100ns.out -r prod100ns.rst -x prod100ns.mdcrd -ref prod90ns.rst
+for ((i=1; i<=10; i++)) ##Loop over equil steps
+do
+    if (i=1)
+    do
+        $Exec -O -i $Step1Dir/prod.in -p $Step1Dir/$Parm -c equil10.rst -o prod($i)0ns.out -r prod($i)0ns.rst -x prod($i)0ns.mdcrd -ref equil10.rst
+    fi
+    $Exec -O -i $Step1Dir/prod.in -p $Step1Dir/$Parm -c prod($i-1)0ns.rst -o prod($i)0ns.out -r prod($i)0ns.rst -x prod($i)0ns.mdcrd -ref prod($i-1)0ns.rst 
+done
+
+## Ultima etapa de equilibrio + produção de 100ns
 
 echo "------------------------------------------------"
 echo "        Replica finalizada com sucesso!         "

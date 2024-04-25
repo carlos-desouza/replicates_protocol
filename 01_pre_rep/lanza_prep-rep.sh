16483aac1b41519453f368d9273da6da901eceed
Carlos Souza <105952734+karurosu28c@users.noreply.github.com>
2024-04-25 19:08:16 -0300

    Update lanza_prep-rep.sh

--- 01_pre_rep/lanza_prep-rep.sh
+++ 01_pre_rep/lanza_prep-rep.sh
@@ -13,34 +13,34 @@ module load md/amber22
 #-----------------------------------#
 export Exec=pmemd.cuda
 export Parm='step3_input.parm7'
-export Topo='step3_input.crd'
+export Topo='step3_input.rst7'
 export minrc=../arq_entrada/01_min
 export equilrc=../arq_entrada/02_equil/300k
 #----------------------------------#
 echo "iniciou teu calculo" ; date
 for ((i=1; i<=7; i++)) ##Loop over min steps
 do
-    if [ i=1 ]
+    if [ $i -eq 1 ]
     then
-        $Exec -O -i $minrc/min($i).in -p $Parm -c $Topo -o min($i).out -r min($i).rst -ref $Topo
+        $Exec -O -i $minrc/min$i.in -p $Parm -c $Topo -o min$i.out -r min$i.rst -ref $Topo
     fi
-    $Exec -O -i $minrc/min($i).in -p $Parm -c min($i-1).rst -o min($i).out -r min($i).rst -ref min($i-1).rst    
+    $Exec -O -i $minrc/min$i.in -p $Parm -c min$(($i-1)).rst -o min$i.out -r min$i.rst -ref min$(($i-1)).rst    
 done
 echo "------------------------------------------------"
 echo "            Minimização terminou!              "
 echo "------------------------------------------------"
 for ((i=1; i<=9; i++)) ##Loop over equil steps
 do
-    if [ i=1 ]
+    if [ $i -eq 1 ]
     then
-        $Exec -O -i $equilrc/equil($i).in -p $Parm -c min7.rst -o equil($i).out -r equil($i).rst -x equil($i).mdcrd -ref min7.rst
+        $Exec -O -i $equilrc/equil$i.in -p $Parm -c min7.rst -o equil$i.out -r equil$i.rst -x equil$i.mdcrd -ref min7.rst
     fi
-    $Exec -O -i $equilrc/equil($i).in -p $Parm -c equil($i-1).rst -o equil($i).out -r equil($i).rst -x equil($i).mdcrd -ref equil($i-1).rst  
+    $Exec -O -i $equilrc/equil$i.in -p $Parm -c equil$(($i-1)).rst -o equil$i.out -r equil$i.rst -x equil$i.mdcrd -ref equil$(($i-1)).rst  
 done
 echo "------------------------------------------------"
 echo "      Aquecimento e equilibrio terminou!        "
 echo "------------------------------------------------"
-cp ../arq_entrada/03_production/prod.in .
+cp ../arq_entrada/03_production/prod.in $equilrc/equil10.in .
 #--------------------------------------------------------------------------------------------------------------------------#
 export inicio=$PWD
 export destino=$PWD/../02_replicas
@@ -53,3 +53,4 @@ done
 echo "------------------------------------------------"
 echo "       Replicas submetidas com sucesso!         "
 echo "------------------------------------------------"
+date

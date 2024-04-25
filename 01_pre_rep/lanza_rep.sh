c4c3aafc9e0478d96facb2bbd04c9596b7cfdd75
Carlos Souza <105952734+karurosu28c@users.noreply.github.com>
2024-04-25 19:08:34 -0300

    Update lanza_rep.sh

--- 01_pre_rep/lanza_rep.sh
+++ 01_pre_rep/lanza_rep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -N pre-rep
+#PBS -N TAL
 #PBS -l select=1:ncpus=1:ngpus=1
 #PBS -l walltime=100:00:00
 #PBS -q CCAD_QGPU
@@ -17,13 +17,13 @@ export Parm=step3_input.parm7
 
 $Exec -O -i $Step1Dir/equil10.in -p $Step1Dir/$Parm -c $Step1Dir/equil9.rst -o equil10.out -r equil10.rst -x equil10.mdcrd -ref $Step1Dir/equil9.rst
 
-for ((i=1; i<=10; i++)) ##Loop over equil steps
+for ((i=1; i<=10; i++)) ##Loop over prod steps
 do
     if (i=1)
     then
-        $Exec -O -i $Step1Dir/prod_constra.in -p $Step1Dir/$Parm -c equil10.rst -o prod($i)0ns.out -r prod($i)0ns.rst -x prod($i)0ns.mdcrd -ref equil10.rst
+        $Exec -O -i $Step1Dir/prod_constra.in -p $Step1Dir/$Parm -c equil10.rst -o prod$(($i))0ns.out -r prod$(($i))0ns.rst -x prod$(($i))0ns.mdcrd -ref equil10.rst    #Last equil
     fi
-    $Exec -O -i $Step1Dir/prod_constra.in -p $Step1Dir/$Parm -c prod($i-1)0ns.rst -o prod($i)0ns.out -r prod($i)0ns.rst -x prod($i)0ns.mdcrd -ref prod($i-1)0ns.rst 
+    $Exec -O -i $Step1Dir/prod_constra.in -p $Step1Dir/$Parm -c prod$(($i-1))0ns.rst -o prod$(($i))0ns.out -r prod$(($i))0ns.rst -x prod$(($i))0ns.mdcrd -ref prod$(($i-1))0ns.rst 
 done
 
 ## Ultima etapa de equilibrio + produção de 100ns

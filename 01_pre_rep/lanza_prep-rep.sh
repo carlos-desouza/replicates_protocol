a575e5ed70e595a582b649670e688ab1b3df4657
Carlos Souza <105952734+karurosu28c@users.noreply.github.com>
2024-04-25 16:58:07 -0300

    Update lanza_prep-rep.sh

--- 01_pre_rep/lanza_prep-rep.sh
+++ 01_pre_rep/lanza_prep-rep.sh
@@ -1,53 +1,42 @@
 #!/bin/bash
-#SBATCH -n 1
-#SBATCH --time 5-00:00:00
-#SBATCH -p gpu
-#SBATCH -J pre-rep
-#--------------------------------------------------------------------------------------------------------------------------#
-Exibe os nós alocados para o Job
-echo $SLURM_JOB_NODELIST
-nodeset -e $SLURM_JOB_NODELIST
-cd $SLURM_SUBMIT_DIR
-#--------------------------------------------------------------------------------------------------------------------------#
-module load md/amber20
-#--------------------------------------------------------------------------------------------------------------------------#
+#PBS -N pre-rep
+#PBS -l select=1:ncpus=1:ngpus=1
+#PBS -l walltime=100:00:00
+#PBS -q CCAD_QGPU
+#PBS -o output.prep
+#PBS -e error.prep
+#-----------------------------------#
+WORKDIR=$PBS_O_WORKDIR
+echo $WORKDIR
+cd $WORKDIR
+module load md/amber22
+#-----------------------------------#
 export Exec=pmemd.cuda
 export Parm='step3_input.parm7'
 export Topo='step3_input.crd'
 export minrc=../arq_entrada/01_min
-export equilrc=../arq_entrada/02_equil/313.15k
-#--------------------------------------------------------------------------------------------------------------------------##
-## Min1
-$Exec -O -i $minrc/min1.in -p $Parm -c $Topo -o min1.out -r min1.rst -ref $Topo
-$Exec -O -i $minrc/min2.in -p $Parm -c min1.rst -o min2.out -r min2.rst -ref min1.rst
-$Exec -O -i $minrc/min3.in -p $Parm -c min2.rst -o min3.out -r min3.rst -ref min2.rst
-$Exec -O -i $minrc/min4.in -p $Parm -c min3.rst -o min4.out -r min4.rst -ref min3.rst
-$Exec -O -i $minrc/min5.in -p $Parm -c min4.rst -o min5.out -r min5.rst -ref min4.rst
-$Exec -O -i $minrc/min6.in -p $Parm -c min5.rst -o min6.out -r min6.rst -ref min5.rst
-$Exec -O -i $minrc/min7.in -p $Parm -c min6.rst -o min7.out -r min7.rst -ref min6.rst
-## Min2
-$Exec -O -i $minrc/min1.in -p $Parm -c min7.rst -o min1.out -r min1.rst -ref min7.rst
-$Exec -O -i $minrc/min2.in -p $Parm -c min1.rst -o min2.out -r min2.rst -ref min1.rst
-$Exec -O -i $minrc/min3.in -p $Parm -c min2.rst -o min3.out -r min3.rst -ref min2.rst
-$Exec -O -i $minrc/min4.in -p $Parm -c min3.rst -o min4.out -r min4.rst -ref min3.rst
-$Exec -O -i $minrc/min5.in -p $Parm -c min4.rst -o min5.out -r min5.rst -ref min4.rst
-$Exec -O -i $minrc/min6.in -p $Parm -c min5.rst -o min6.out -r min6.rst -ref min5.rst
-$Exec -O -i $minrc/min7.in -p $Parm -c min6.rst -o min7.out -r min7.rst -ref min6.rst
-#--------------------------------------------------------------------------------------------------------------------------#
+export equilrc=../arq_entrada/02_equil/300k
+#----------------------------------#
+echo "iniciou teu calculo" ; date
+for ((i=1; i<=7; i++)) ##Loop over min steps
+do
+    if (i=1)
+    do
+        $Exec -O -i $minrc/min($i).in -p $Parm -c $Topo -o min1.out -r min1.rst -ref $Topo
+    fi
+    $Exec -O -i $minrc/min($i).in -p $Parm -c min($i-1).rst -o min($i).out -r min($i).rst -ref min($i-1).rst    
+done
 echo "------------------------------------------------"
-echo "      Minimização em duplicata terminou!        "
+echo "            Minimização terminou!              "
 echo "------------------------------------------------"
-## Aquecimento e equilibrio em 10 etapas
-$Exec -O -i $equilrc/equil1.in -p $Parm -c min7.rst -o equil1.out -r equil1.rst -x equil1.mdcrd -ref min7.rst
-$Exec -O -i $equilrc/equil2.in -p $Parm -c equil1.rst -o equil2.out -r equil2.rst -x equil2.mdcrd -ref equil1.rst
-$Exec -O -i $equilrc/equil3.in -p $Parm -c equil2.rst -o equil3.out -r equil3.rst -x equil3.mdcrd -ref equil2.rst
-$Exec -O -i $equilrc/equil4.in -p $Parm -c equil3.rst -o equil4.out -r equil4.rst -x equil4.mdcrd -ref equil3.rst
-$Exec -O -i $equilrc/equil5.in -p $Parm -c equil4.rst -o equil5.out -r equil5.rst -x equil5.mdcrd -ref equil4.rst
-$Exec -O -i $equilrc/equil6.in -p $Parm -c equil5.rst -o equil6.out -r equil6.rst -x equil6.mdcrd -ref equil5.rst
-$Exec -O -i $equilrc/equil7.in -p $Parm -c equil6.rst -o equil7.out -r equil7.rst -x equil7.mdcrd -ref equil6.rst
-$Exec -O -i $equilrc/equil8.in -p $Parm -c equil7.rst -o equil8.out -r equil8.rst -x equil8.mdcrd -ref equil7.rst 
-$Exec -O -i $equilrc/equil9.in -p $Parm -c equil8.rst -o equil9.out -r equil9.rst -x equil9.mdcrd -ref equil8.rst
-$Exec -O -i $equilrc/equil10.in -p $Parm -c equil9.rst -o equil10.out -r equil10.rst -x equil10.mdcrd -ref equil9.rst
+for ((i=1; i<=9; i++)) ##Loop over equil steps
+do
+    if (i=1)
+    do
+        $Exec -O -i $equilrc/equil($i).in -p $Parm -c min7.rst -o equil($i).out -r equil($i).rst -x equil($i).mdcrd -ref min7.rst
+    fi
+    $Exec -O -i $equilrc/equil($i).in -p $Parm -c equil($i-1).rst -o equil($i).out -r equil($i).rst -x equil($i).mdcrd -ref equil($i-1).rst  
+done
 echo "------------------------------------------------"
 echo "      Aquecimento e equilibrio terminou!        "
 echo "------------------------------------------------"
@@ -55,11 +44,11 @@ cp ../arq_entrada/03_production/prod.in .
 #--------------------------------------------------------------------------------------------------------------------------#
 export inicio=$PWD
 export destino=$PWD/../02_replicas
-for ((i=1; i<=2; i++))                 # i é o número de replicas... mudar se precisar
+for ((i=1; i<=10; i++)) # i é o número de replicas... mudar se precisar
 do
     mkdir $destino/rep$i ; cp lanza_rep.sh $destino/rep$i/
     sed -i "s/TAL/$i/g" "$destino/rep$i/lanza_rep.sh"
-    cd $destino/rep$i ; sbatch $destino/rep$i/lanza_rep.sh ; cd $inicio
+    cd $destino/rep$i ; qsub $destino/rep$i/lanza_rep.sh ; cd $inicio
 done
 echo "------------------------------------------------"
 echo "       Replicas submetidas com sucesso!         "

e4aeb90fe69a83b5860a4c8bcdfbe2f3a9114b1e
Carlos Souza <105952734+karurosu28c@users.noreply.github.com>
2024-09-02 18:50:56 -0300

    Update lanza_rep.sh

--- 01_pre_rep/lanza_rep.sh
+++ 01_pre_rep/lanza_rep.sh
@@ -10,25 +10,21 @@ WORKDIR=$PBS_O_WORKDIR
 echo $WORKDIR
 cd $WORKDIR
 module load md/amber22
-
 export Step1Dir=../../01_pre_rep
 export Exec=pmemd.cuda
 export Parm=step3_input.parm7
 
 $Exec -O -i $Step1Dir/equil10.in -p $Step1Dir/$Parm -c $Step1Dir/equil9.rst -o equil10.out -r equil10.rst -x equil10.mdcrd -ref $Step1Dir/equil9.rst
-
-for ((i=1; i<=10; i++)) ##Loop over prod steps
+for ((i=1; i<=10; i++))
 do
-    if [ $i -eq 1 ]
-    then
-        $Exec -O -i $Step1Dir/prod_constra.in -p $Step1Dir/$Parm -c equil10.rst -o prod$(($i))0ns.out -r prod$(($i))0ns.rst -x prod$(($i))0ns.mdcrd -ref equil10.rst    #first 
-    fi
-    if [ $i -gt 1 ]
-    then
-        $Exec -O -i $Step1Dir/prod_constra.in -p $Step1Dir/$Parm -c prod$(($i-1))0ns.rst -o prod$(($i))0ns.out -r prod$(($i))0ns.rst -x prod$(($i))0ns.mdcrd -ref prod$(($i-1))0ns.rst ## Prod 
+    if [ $i -eq 1 ]; then
+        $Exec -O -i $Step1Dir/prod_constra.in -p $Step1Dir/$Parm -c equil10.rst \
+              -o prod${i}0ns.out -r prod${i}0ns.rst -x prod${i}0ns.mdcrd -ref equil10.rst
+    else
+        $Exec -O -i $Step1Dir/prod_constra.in -p $Step1Dir/$Parm -c prod$((i-1))0ns.rst \
+              -o prod${i}0ns.out -r prod${i}0ns.rst -x prod${i}0ns.mdcrd -ref prod$((i-1))0ns.rst
     fi
 done
-
 ## Ultima etapa de equilibrio + produção de 100ns
 
 echo "------------------------------------------------"

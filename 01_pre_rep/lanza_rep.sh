facd52599a6f56585935255d0af4257e6333f51e
Carlos Souza <105952734+carlos-desouza@users.noreply.github.com>
2025-11-25 10:14:20 -0300

    Migrate job scheduler from PBS to SLURM

--- 01_pre_rep/lanza_rep.sh
+++ 01_pre_rep/lanza_rep.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-#PBS -N TAL
-#PBS -l select=1:ncpus=1:ngpus=1
-#PBS -l walltime=100:00:00
-#PBS -q CCAD_QGPU
-#PBS -o output.prep
-#PBS -e error.prep
-#-----------------------------------#
-WORKDIR=$PBS_O_WORKDIR
-echo $WORKDIR
-cd $WORKDIR
-module load md/amber22
-export Step1Dir=../../01_pre_rep
-export Exec=pmemd.cuda
-export Parm=step3_input.parm7
-
-$Exec -O -i $Step1Dir/equil10.in -p $Step1Dir/$Parm -c $Step1Dir/equil9.rst -o equil10.out -r equil10.rst -x equil10.mdcrd -ref $Step1Dir/equil9.rst
-#!/bin/bash
 #SBATCH --job-name=BsEst_repTAL   # Nome do job
 #SBATCH --nodes=1                          # Numero de nós
 #SBATCH --ntasks=1                         # Numero de tarefas (uma única tarefa)
@@ -50,11 +33,6 @@ EOF
         sleep 2
     fi
 done
-## Ultima etapa de equilibrio + produção de 100ns
-
-echo "------------------------------------------------"
-echo "        Replica finalizada com sucesso!         "
-echo "------------------------------------------------"
 ## Ultima etapa de equilibrio + produção de 100ns
 
 echo "------------------------------------------------"
